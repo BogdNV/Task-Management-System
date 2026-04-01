03963027a0bedfae7c7d61546f2bb9a98c3a6ad6
Bogd <nikolaevbv92@gmail.com>
2026-04-01 19:09:31 +0300

    добавить методы фильтрации

--- src/TaskManager.Domain/Interfaces/ITaskRepository.cs
+++ src/TaskManager.Domain/Interfaces/ITaskRepository.cs
@@ -1,4 +1,5 @@
 using TaskManager.Domain.Entities;
+using TaskManager.Domain.Enums;
 
 namespace TaskManager.Domain.Interfaces;
 
@@ -6,6 +7,8 @@ public interface ITaskRepository
 {
     Task<TaskItem?> GetByIdAsync(int id);
     Task<IEnumerable<TaskItem>> GetAllAsync();
+    Task<IEnumerable<TaskItem>> GetByProjectIdAsync(int projectId);
+    Task<IEnumerable<TaskItem>> GetByStatus(Status status);
     Task AddAsync(TaskItem task);
     Task UpdateAsync(TaskItem task);
     Task DeleteAsync(int id);
