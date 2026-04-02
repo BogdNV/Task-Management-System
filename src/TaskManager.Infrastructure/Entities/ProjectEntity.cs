cb9aad9e8164d3b156c627b45555921a7dd99c6f
Bogd <nikolaevbv92@gmail.com>
2026-04-02 14:41:31 +0300

    добавить ProjectEntity

---
+++ src/TaskManager.Infrastructure/Entities/ProjectEntity.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Infrastructure.Entities;
+
+public class ProjectEntity
+{
+    public int Id { get; set; }
+    [Required]
+    [MinLength(5)]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    [Required]
+    public int OwnerId { get; set; }
+}
