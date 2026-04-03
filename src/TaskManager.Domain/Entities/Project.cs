b0a29b91c1c2a45aebf9fc72501027eb6ee3658f
Bogd <nikolaevbv92@gmail.com>
2026-04-03 14:09:32 +0300

    добавить валидацию описания в Project

--- src/TaskManager.Domain/Entities/Project.cs
+++ src/TaskManager.Domain/Entities/Project.cs
@@ -4,6 +4,7 @@ public class Project
 {
     public const int MAX_NAME_LENGTH = 100;
     public const int MIN_NAME_LENGTH = 5;
+    public const int MAX_DESCRIPTION_LENGTH = 1000;
     public int Id { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
@@ -46,11 +47,27 @@ public class Project
         Name = newName;
     }
 
+    public void UpdateDescription(string newDescription)
+    {
+        ValidateDescription(newDescription);
+        Description = newDescription;
+    }
+
     private static void ValidateName(string name)
     {
         if (string.IsNullOrEmpty(name))
             throw new ArgumentException("Название не должно быть пустым", nameof(name));
         if (name.Length < 5)
             throw new ArgumentException($"Название должно быть длиной мнимум {MIN_NAME_LENGTH} символов", nameof(name));
+    }
+
+    private static void ValidateDescription(string description)
+    {
+        if (string.IsNullOrEmpty(description))
+            throw new ArgumentException("Описание не должно быть пустым", nameof(description));
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Описание состоит из пробелов", nameof(description));
+        if (description.Length > 1000)
+            throw new ArgumentException($"Превышен порог в {MAX_DESCRIPTION_LENGTH} символов", nameof(description));
     }
 }
