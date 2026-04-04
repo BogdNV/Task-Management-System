ae929b031978bd596e1a7bbcbc65259ac313d790
Bogd <nikolaevbv92@gmail.com>
2026-04-04 20:34:03 +0300

    настройка приложения в Program.cs

--- src/TaskManager.API/Program.cs
+++ src/TaskManager.API/Program.cs
@@ -1,3 +1,7 @@
+using TaskManager.API.Middleware;
+using TaskManager.Application.Extensions;
+using TaskManager.Infrastructure.Extensions;
+
 namespace TaskManager.API;
 
 public class Program
@@ -5,17 +9,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
         builder.Services.AddControllers();
-        builder.Services.AddOpenApi();
+        var connectionString = builder.Configuration.GetConnectionString("PostgreSqlConnection");
+        builder.Services.AddInfrastructure(connectionString);
+        builder.Services.AddApplication(builder);
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
 
         app.MapControllers();
 
