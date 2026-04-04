d802cbb10ddcc999c9db4d1d407487c0bee398fc
Bogd <nikolaevbv92@gmail.com>
2026-04-04 20:17:27 +0300

    добавить MediatR в DI

--- src/TaskManager.Application/Extensions/DependencyInjectionExtensions.cs
+++ src/TaskManager.Application/Extensions/DependencyInjectionExtensions.cs
@@ -1,14 +1,27 @@
 using System.Reflection;
 using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Application.Common.Behaviors;
 
 namespace TaskManager.Application.Extensions;
 
 public static class DependencyInjectionExtensions
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplication(this IServiceCollection services, WebApplicationBuilder builder)
     {
+        services.AddMediatR(cfg =>
+            {
+                cfg.LicenseKey = builder.Configuration["MEDIATR_LICENSE_KEY"];
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+            });
+
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
         return services;
     }
